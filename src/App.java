57113dc348028f6a8ef9d56a1a8fae4e34243b15
TrabalhoPoo2023 <trabalhopoo.2023@gmail.com>
2024-04-17 20:52:24 -0400

    questão prova

---
+++ src/App.java
@@ -0,0 +1,20 @@
+import prova.Circulo;
+import prova.Quadrado;
+import prova.Retangulo;
+import prova.Triangulo;
+public class App {
+    public static void main(String[] args) throws Exception {
+       Quadrado quadrado = new Quadrado();
+       quadrado.toString();
+
+       Triangulo triangulo = new Triangulo();
+       triangulo.toString();
+
+       Retangulo retangulo = new Retangulo();
+       retangulo.toString();
+
+       Circulo circulo = new Circulo();
+       circulo.toString();
+
+    }
+}
