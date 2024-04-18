01311112796e96f1655ed1d0d1a3391ca26e6b17
TrabalhoPoo2023 <trabalhopoo.2023@gmail.com>
2024-04-17 20:59:41 -0400

    .

--- src/App.java
+++ src/App.java
@@ -5,15 +5,26 @@ import prova.Triangulo;
 public class App {
     public static void main(String[] args) throws Exception {
        Quadrado quadrado = new Quadrado();
+       quadrado.setLado1(5);
+       quadrado.setLado2(5);
+       quadrado.setCor("azul");
        quadrado.toString();
 
        Triangulo triangulo = new Triangulo();
+       triangulo.setAltura(3);
+       triangulo.setBase(8);
+       triangulo.setCor("preto");
        triangulo.toString();
 
        Retangulo retangulo = new Retangulo();
+       retangulo.setLado1(6);
+       retangulo.setLado2(4);
+       retangulo.setCor("vermelho");
        retangulo.toString();
 
        Circulo circulo = new Circulo();
+       circulo.setRaio(6);
+       circulo.setCor("branco");
        circulo.toString();
 
     }
--- src/prova/Quadrado.java
+++ src/prova/Quadrado.java
@@ -4,49 +4,41 @@ public class Quadrado extends Retangulo {
 
     @Override
     public double getArea() {
-        // TODO Auto-generated method stub
         return super.getArea();
     }
 
     @Override
     public double getLado1() {
-        // TODO Auto-generated method stub
         return super.getLado1();
     }
 
     @Override
     public double getLado2() {
-        // TODO Auto-generated method stub
         return super.getLado2();
     }
 
     @Override
     public void setLado1(double lado1) {
-        // TODO Auto-generated method stub
         super.setLado1(lado1);
     }
 
     @Override
     public void setLado2(double lado2) {
-        // TODO Auto-generated method stub
         super.setLado2(lado2);
     }
 
     @Override
     public String toString() {
-        // TODO Auto-generated method stub
         return super.toString();
     }
 
     @Override
     protected String getCor() {
-        // TODO Auto-generated method stub
         return super.getCor();
     }
 
     @Override
     public void setCor(String cor) {
-        // TODO Auto-generated method stub
         super.setCor(cor);
     }
 
